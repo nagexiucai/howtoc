ba1eea11783773a76150a822b21b5001b336c622
xiucai <xiucai@pahuashan.cn>
2016-11-08 11:55:43 +0800

    first commit

---
+++ samples/ftp_download.c
@@ -0,0 +1,18 @@
+/*
+ * ftp_download.c
+ *
+ *  Created on: 10.10.2016
+ *  Author    : nagexiucai
+ */
+
+#include "../build.h"
+
+#if SAMPLES_FTP_DOWNLOAD
+
+int main(void)
+{
+	printf("#####SAMPLES FTP DOWNLOAD#####");
+	return 0;
+}
+
+#endif
---
+++ samples/ftp_upload.c
@@ -0,0 +1,19 @@
+/*
+ * ftp_upload.c
+ *
+ *  Created on: 10.19.2016
+ *      Author: nagexiucai
+ */
+
+#include "../build.h"
+
+#if SAMPLES_FTP_UPLOAD
+
+int main(void)
+{
+	printf("#####SAMPLES FTP UPLOAD#####");
+	return 0;
+}
+
+#endif
+
